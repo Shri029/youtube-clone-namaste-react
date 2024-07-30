dcf2f2af2201cde055596d8db0c72bc27062b01b
Shivani Tiwary <shivanit.mcs17.du@gmail.com>
2024-07-30 15:27:59 +0530

    Removed API_KEY

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,2 +1,1 @@
-const GOOGLE_API_KEY = "AIzaSyD1gaLwayYV9shPsnEUKUt9r8KiYEnEb04";
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=60&regionCode=IN&key="+GOOGLE_API_KEY;
+// export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=60&regionCode=IN&key="+GOOGLE_API_KEY;
